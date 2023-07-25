52df9de58d78481c517b624ad37d3c9014320385
xben00 <benlouisdietz@live.de>
2023-07-25 10:10:47 +0200

    Update PlannerScreen.kt
    not working yet

--- app/src/main/java/de/kassel/cc22023/roadtrip/ui/planner/PlannerScreen.kt
+++ app/src/main/java/de/kassel/cc22023/roadtrip/ui/planner/PlannerScreen.kt
@@ -1,5 +1,6 @@
 package de.kassel.cc22023.roadtrip.ui.planner
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 
@@ -9,8 +10,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerState
 
 import androidx.compose.material3.DateRangePicker
+import androidx.compose.material3.DateRangePickerState
+import androidx.compose.material3.DisplayMode
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,71 +25,118 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberDatePickerState
 
 import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.util.Calendar
+import java.util.Date
 
 @ExperimentalMaterial3Api
 @Composable
 fun PlannerScreen() {
+    val state = rememberDatePickerState(initialSelectedDateMillis = Calendar.getInstance().timeInMillis )
+    Column(Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        var date by remember { mutableStateOf(LocalDate.now().toString()) }
+        TextField(value = date,
+            onValueChange = {date =it},
+        label = {Text("Start Date")})
+        //DateInputSample(state)
+
+        //Text(state.selectedDateMillis.toString())
+    }
+
+
+
+    /*
+    val startDate = LocalDate.now()
+    val endDate = startDate
     Column(Modifier.fillMaxWidth(),
     horizontalAlignment = Alignment.CenterHorizontally) {
 
+
+
+
+        DatePickerSample()
+
     }
-    Text(text = "Planner")
-    DateRangePickerSample()
-
-
+*/
 }
-@ExperimentalMaterial3Api
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DateRangePickerSample() {
+fun DatePickerSample(state:DatePickerState) {
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+
+
+        DatePicker(state = state, modifier = Modifier.padding(16.dp))
+
+        //Text("Selected date timestamp: ${datePickerState.selectedDateMillis ?: "no selection"}")
+    }
+}
+@SuppressLint("UnrememberedMutableState")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerDialogSample(state:DatePickerState) {
     // Decoupled snackbar host state from scaffold state for demo purposes.
     val snackState = remember { SnackbarHostState() }
     val snackScope = rememberCoroutineScope()
-    SnackbarHost(hostState = snackState, Modifier.zIndex(1f))
+    SnackbarHost(hostState = snackState, Modifier)
+    val openDialog = remember { mutableStateOf(true) }
+    // TODO demo how to read the selected date from the state.
+    if (openDialog.value) {
 
-    val state = rememberDateRangePickerState()
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Top) {
-        // Add a row with "Save" and dismiss actions.
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 12.dp, end = 12.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            IconButton(onClick = { /* dismiss the UI */ }) {
-                Icon(Icons.Filled.Close, contentDescription = "Localized description")
-            }
-            TextButton(
-                onClick = {
-                    snackScope.launch {
-                        snackState.showSnackbar(
-                            "Saved range (timestamps): " +
-                                    "${state.selectedStartDateMillis!!..state.selectedEndDateMillis!!}"
-                        )
+        val confirmEnabled = derivedStateOf { state.selectedDateMillis != null }
+        DatePickerDialog(
+            onDismissRequest = {
+                // Dismiss the dialog when the user clicks outside the dialog or on the back
+                // button. If you want to disable that functionality, simply use an empty
+                // onDismissRequest.
+                openDialog.value = false
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        openDialog.value = false
+                        snackScope.launch {
+                            snackState.showSnackbar(
+                                "Selected date timestamp: ${state.selectedDateMillis}"
+                            )
+                        }
+                    },
+                    enabled = confirmEnabled.value
+                ) {
+                    Text("OK")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        openDialog.value = false
                     }
-                },
-                enabled = state.selectedEndDateMillis != null
-            ) {
-                Text(text = "Save")
+                ) {
+                    Text("Cancel")
+                }
             }
+        ) {
+            DatePicker(state = state)
         }
-
-        DateRangePicker(state = state, modifier = Modifier.weight(1f))
     }
 }
 
-
-
-
-    
