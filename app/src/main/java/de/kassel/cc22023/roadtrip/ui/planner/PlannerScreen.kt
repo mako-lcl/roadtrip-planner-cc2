1396a34753a075d8a25ad6069fcadadbec83386d
xben00 <benlouisdietz@live.de>
2023-07-26 11:49:53 +0200

    input validation

--- app/src/main/java/de/kassel/cc22023/roadtrip/ui/planner/PlannerScreen.kt
+++ app/src/main/java/de/kassel/cc22023/roadtrip/ui/planner/PlannerScreen.kt
@@ -2,6 +2,7 @@ package de.kassel.cc22023.roadtrip.ui.planner
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 
 import androidx.compose.foundation.layout.Column
@@ -18,6 +19,7 @@ import androidx.compose.foundation.layout.width
 
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 
 
@@ -33,11 +35,13 @@ import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberDatePickerState
 
 
@@ -63,6 +67,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -81,18 +86,13 @@ import kotlinx.coroutines.launch
 import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneOffset
+import kotlin.reflect.jvm.internal.impl.descriptors.deserialization.PlatformDependentDeclarationFilter.All
 
 
 @ExperimentalMaterial3Api
 @Composable
 fun PlannerScreen(viewModel: PlannerViewModel = hiltViewModel()) {
     val context = LocalContext.current
-
-
-
-
-
-
 
 
     var selectedStartDate by remember { mutableStateOf(LocalDate.now()) }
@@ -109,7 +109,12 @@ fun PlannerScreen(viewModel: PlannerViewModel = hiltViewModel()) {
     }
     var startDate by remember { mutableStateOf(selectedStartDate.toString()) }
     var endDate by remember { mutableStateOf(selectedEndDate.toString()) }
+    var startDateError by remember { mutableStateOf(false) }
+    var endDateError by remember { mutableStateOf(false) }
+    var endLocationError by remember { mutableStateOf(false) }
+    var startLocationError by remember { mutableStateOf(false) }
     val currentDate = remember { LocalDate.now() }
+
 
     Column(
         Modifier
@@ -125,36 +130,84 @@ fun PlannerScreen(viewModel: PlannerViewModel = hiltViewModel()) {
             modifier = Modifier.fillMaxWidth()
         ) {
             TextField(
+
                 value = startLocation,
-                onValueChange = { startLocation = it },
-                label = { Text("Start Location") },
-                modifier = Modifier.weight(1.5f),
-                singleLine = true
+                onValueChange = {
+                    startLocation = it
+                    startLocationError = startLocation == ""
+                },
+                label = { Text(text = "Start",maxLines = 1) },
+
+                modifier = Modifier.weight(1f),
+                singleLine = true,
+                isError = startLocationError,
+                supportingText = {
+                    if (startLocationError) {
+                        Text(text = "Can not be empty")
+                    }
+                },
+                trailingIcon = {
+                    if (startLocationError) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.error),
+                            contentDescription = "Trailing Icon"
+                        )
+                    }
+                }
+
+
             )
             Spacer(modifier = Modifier.width(5.dp))
 
             Box(
                 modifier = Modifier.weight(1f)
             ) {
-                Text(
-                    text = startDate,
-                    //fontSize = 16.sp,
-                    modifier = Modifier.fillMaxWidth()
-                )
+                TextField(
+                    value = startDate,
+                    onValueChange = {},
+                    singleLine = true,
+                    enabled = false,
+                    modifier = Modifier.clickable(onClick = { showStartDatePicker = true }),
+                    label = { Text(text = "Start Date") },
+                    colors = TextFieldDefaults.colors(
+
+                        disabledContainerColor = if (!startDateError) colorScheme.primaryContainer else colorScheme.errorContainer,
+                        disabledTextColor = if (!startDateError) colorScheme.onPrimaryContainer else colorScheme.error,
+                        disabledTrailingIconColor = if (!startDateError) colorScheme.onPrimaryContainer else colorScheme.error,
+                        disabledSupportingTextColor = colorScheme.error,
+                        //errorContainerColor = colorScheme.error,
+                        errorTextColor = colorScheme.error
+
+
+                    ),
+                    trailingIcon = {
+                        if (!startDateError) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.baseline_calendar_month_24),
+                                contentDescription = "Trailing Icon"
+                            )
+                        } else {
+                            Icon(
+                                painter = painterResource(id = R.drawable.error),
+                                contentDescription = "Trailing Icon"
+                            )
+                        }
+                    },
+                    supportingText = {
+                        if (startDateError) {
+                            Text(text = "Can not be in the past")
+                        }
+                    },
+                    isError = startDateError,
+
+
+                    )
+
+
             }
 
-            Button(
-                onClick = { showStartDatePicker = true },
-
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.baseline_calendar_month_24),
-                    contentDescription = null,
-                    modifier = Modifier.size(20.dp),
-                    colorFilter = ColorFilter.tint(color = Color.White)
-                )
-            }
         }
+
 
         if (showStartDatePicker) {
             DatePickerDialogSample(
@@ -166,6 +219,9 @@ fun PlannerScreen(viewModel: PlannerViewModel = hiltViewModel()) {
                         Instant.ofEpochMilli(newDate).atZone(ZoneOffset.UTC).toLocalDate()
                     startDate = selectedStartDate.toString()
                     showStartDatePicker = false
+                    startDateError = selectedStartDate.isBefore(currentDate)
+                    endDateError = selectedEndDate.isBefore(selectedStartDate)
+                    /*
                     if (selectedStartDate.isBefore(currentDate)) {
                         selectedStartDate = currentDate
                         startDate = currentDate.toString()
@@ -174,7 +230,7 @@ fun PlannerScreen(viewModel: PlannerViewModel = hiltViewModel()) {
                         // If the endDate is before the startDate, reset the endDate to match the startDate
                         selectedEndDate = selectedStartDate
                         endDate = selectedStartDate.toString()
-                    }
+                    }*/
                 },
                 onCloseDialog = { showStartDatePicker = false }
             )
@@ -190,10 +246,25 @@ fun PlannerScreen(viewModel: PlannerViewModel = hiltViewModel()) {
         ) {
             TextField(
                 value = endLocation,
-                onValueChange = { endLocation = it },
-                label = { Text("End Location") },
-                modifier = Modifier.weight(1.5f),
-                singleLine = true
+                onValueChange = { endLocation = it
+                                endLocationError = endLocation == ""},
+                label = { Text("Destination") },
+                modifier = Modifier.weight(1f),
+                singleLine = true,
+                isError = endLocationError,
+                supportingText = {
+                    if (endLocationError) {
+                        Text(text = "Can not be empty")
+                    }
+                },
+                trailingIcon = {
+                    if (endLocationError) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.error),
+                            contentDescription = "Trailing Icon"
+                        )
+                    }
+                }
 
             )
             Spacer(modifier = Modifier.width(5.dp))
@@ -201,23 +272,47 @@ fun PlannerScreen(viewModel: PlannerViewModel = hiltViewModel()) {
             Box(
                 modifier = Modifier.weight(1f)
             ) {
-                Text(
-                    text = endDate,
-                    //fontSize = 16.sp,
-                    modifier = Modifier.fillMaxWidth()
-                )
-            }
+                TextField(
+                    value = endDate,
+                    onValueChange = {},
+                    singleLine = true,
+                    enabled = false,
+                    modifier = Modifier.clickable(onClick = { showEndDatePicker = true }),
+                    label = { Text(text = "End Date") },
+                    colors = TextFieldDefaults.colors(
 
-            Button(
-                onClick = { showEndDatePicker = true },
-                //modifier = Modifier.weight(1f)
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.baseline_calendar_month_24),
-                    contentDescription = null,
-                    modifier = Modifier.size(20.dp),
-                    colorFilter = ColorFilter.tint(color = Color.White)
-                )
+                        disabledContainerColor = if (!endDateError) colorScheme.primaryContainer else colorScheme.errorContainer,
+                        disabledTextColor = if (!endDateError) colorScheme.onPrimaryContainer else colorScheme.error,
+                        disabledTrailingIconColor = if (!endDateError) colorScheme.onPrimaryContainer else colorScheme.error,
+                        disabledSupportingTextColor = colorScheme.error,
+                       // errorContainerColor = colorScheme.error,
+                        errorTextColor = colorScheme.error
+
+
+                    ),
+                    trailingIcon = {
+                        if (!endDateError) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.baseline_calendar_month_24),
+                                contentDescription = "Trailing Icon"
+                            )
+                        } else {
+                            Icon(
+                                painter = painterResource(id = R.drawable.error),
+                                contentDescription = "Trailing Icon"
+                            )
+                        }
+                    },
+                    supportingText = {
+                        if (endDateError) {
+                            Text(text = "Has to be after the Start Date")
+                        }
+                    },
+                    isError = endDateError,
+
+
+                    )
+
             }
         }
 
@@ -231,11 +326,17 @@ fun PlannerScreen(viewModel: PlannerViewModel = hiltViewModel()) {
                         Instant.ofEpochMilli(newDate).atZone(ZoneOffset.UTC).toLocalDate()
                     endDate = selectedEndDate.toString()
                     showEndDatePicker = false
+                    endDateError = selectedEndDate.isBefore(selectedStartDate)
+                    /*
+
                     if (selectedEndDate.isBefore(selectedStartDate)) {
                         // If the endDate is before the startDate, reset the endDate to match the startDate
                         selectedEndDate = selectedStartDate
                         endDate = selectedStartDate.toString()
                     }
+                    */
+
+
                 },
 
                 onCloseDialog = { showEndDatePicker = false }
@@ -246,7 +347,11 @@ fun PlannerScreen(viewModel: PlannerViewModel = hiltViewModel()) {
 
 
         Spacer(modifier = Modifier.height(40.dp))
-        ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = { expanded = it },Modifier.fillMaxWidth()) {
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = it },
+            Modifier.fillMaxWidth()
+        ) {
             TextField(
                 value = selectedText,
                 onValueChange = {},
@@ -273,11 +378,13 @@ fun PlannerScreen(viewModel: PlannerViewModel = hiltViewModel()) {
                 }
             }
         }
-        Button(onClick = {
-            val testTrip = loadRoadtripFromAssets(context)
-            val trip = convertRoadtripFromTestTrip(testTrip)
-            viewModel.insertNewRoadtrip(trip)
-        }) {
+        Button(
+            onClick = {
+                val testTrip = loadRoadtripFromAssets(context)
+                val trip = convertRoadtripFromTestTrip(testTrip)
+                viewModel.insertNewRoadtrip(trip)
+            },
+        ) {
             Text(text = "Load")
 
         }
@@ -293,7 +400,7 @@ fun PlannerScreen(viewModel: PlannerViewModel = hiltViewModel()) {
 fun DatePickerDialogSample(
     state: DatePickerState,
     onDateSelected: (Long) -> Unit,
-    onCloseDialog:() ->Unit
+    onCloseDialog: () -> Unit
 ) {
     // Decoupled snackbar host state from scaffold state for demo purposes.
     val snackState = remember { SnackbarHostState() }
@@ -309,7 +416,8 @@ fun DatePickerDialogSample(
                 // Dismiss the dialog when the user clicks outside the dialog or on the back
                 // button. If you want to disable that functionality, simply use an empty
                 // onDismissRequest.
-                //openDialog.value = false
+                openDialog.value = false
+                onCloseDialog()
             },
 
             confirmButton = {
