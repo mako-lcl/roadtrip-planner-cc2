fe1dd1b34b02de990ab8b1be8202c723b7ee8102
Ludwig Chen Li <ludwig.chen.li@comtec.eecs.uni-kassel.de>
2023-07-26 17:23:23 +0200

    added cooler loading animation

--- app/src/main/java/de/kassel/cc22023/roadtrip/ui/util/LoadingScreen.kt
+++ app/src/main/java/de/kassel/cc22023/roadtrip/ui/util/LoadingScreen.kt
@@ -6,9 +6,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import de.kassel.cc22023.roadtrip.R
 import de.kassel.cc22023.roadtrip.ui.theme.MyApplicationTheme
 
 @Composable
@@ -27,6 +34,25 @@ fun LoadingScreen() {
 @Composable
 private fun DefaultPreview() {
     MyApplicationTheme {
-        LoadingScreen()
+        CoolLoadingScreen()
+    }
+}
+
+@Composable
+fun CoolLoadingScreen() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading))
+    val progress by animateLottieCompositionAsState(composition = composition, iterations = LottieConstants.IterateForever)
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        LottieAnimation(
+            composition = composition,
+            progress = { progress },
+        )
+
+        Text("Loading...")
     }
 }
