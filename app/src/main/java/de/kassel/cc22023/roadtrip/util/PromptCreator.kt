28bb7ed49215b60cdef4c8fa90dea04e1c888569
xben00 <benlouisdietz@live.de>
2023-07-27 14:09:23 +0200

    try to fix prompt

--- app/src/main/java/de/kassel/cc22023/roadtrip/util/PromptCreator.kt
+++ app/src/main/java/de/kassel/cc22023/roadtrip/util/PromptCreator.kt
@@ -5,6 +5,8 @@ fun createRoadtripPrompt(startLocation: String, endLocation: String, startDate: 
             "$startLocation to $endLocation from $startDate to $endDate $transportation. The trip should contain" +
             " a packing list named packing_list as an array of items, the start date as start_date, " +
             "the end date as end_date, the start location name as start_location, the end loation name as end_location," +
-            " if the trip is possible as is_possible and a list of locations with their name, latitude, longitude and possible" +
-            " activites as a list of names. Reduce the number of stops to a maximum of 10."
+            " if the trip is possible as is_possible and a list of locations with their name, latitude, longitude, the dat you are there and possible" +
+            " activities as a list of names, with the field_names name, latitude, longitude, date, activities."+
+            "Give one location for each day from $startDate to $endDate, last day inclusive. Check that all dates " +
+            "are covered and that you arrive on the $endDate at $endLocation ."
 }
