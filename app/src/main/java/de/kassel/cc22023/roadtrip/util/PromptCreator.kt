7811fc9b2ead4a705701b27a80f90f8ac5f027e5
Ben Louis Dietz <benlouisdietz@live.de>
2023-08-23 13:59:32 +0200

    Update PromptCreator.kt

--- app/src/main/java/de/kassel/cc22023/roadtrip/util/PromptCreator.kt
+++ app/src/main/java/de/kassel/cc22023/roadtrip/util/PromptCreator.kt
@@ -5,9 +5,9 @@ fun createRoadtripPrompt(startLocation: String, endLocation: String, startDate: 
             "$startLocation to $endLocation from $startDate to $endDate $transportation. The trip should contain" +
             " a packing list named packing_list as an array of items, the start date as start_date, " +
             "the end date as end_date, the start location name as start_location, the end location name as end_location," +
-            " if the trip is possible as is_possible and a list of locations with their name, latitude, longitude, the date you are there, " +
+            " if it´s possible to get in the given time from $startDate to $endDate $transportation as is_possible, make sure that it is false if it isn´t possible and a list of locations with their name, latitude, longitude, the date you are there, " +
             "a boolean that marks 2-3 must have stops that arent the start or end location " +
             "and possible activities as a list of names, with the field_names name, latitude, longitude, date, must_have,  activities."+
             "Make sure that there are multiple activities for each location. Give one location for each day from $startDate to $endDate. Check that all dates " +
-            "are covered and that you arrive on the $endDate at $endLocation. Also check that the distances between locations are below 10 hours"
+            "are covered and that you arrive on the $endDate at $endLocation. Also check that the distances between locations are below 10 hours. "
 }
