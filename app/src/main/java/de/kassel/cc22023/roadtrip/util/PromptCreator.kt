418513bd18c3e871174ae7db1c93c4cb3cd3acf9
Ben Louis Dietz <benlouisdietz@live.de>
2023-08-23 12:57:36 +0200

    added must have and fixed activitiy ui

--- app/src/main/java/de/kassel/cc22023/roadtrip/util/PromptCreator.kt
+++ app/src/main/java/de/kassel/cc22023/roadtrip/util/PromptCreator.kt
@@ -4,9 +4,10 @@ fun createRoadtripPrompt(startLocation: String, endLocation: String, startDate: 
     return "You are a roadtrip planner. Format the response as a json. Plan a trip from " +
             "$startLocation to $endLocation from $startDate to $endDate $transportation. The trip should contain" +
             " a packing list named packing_list as an array of items, the start date as start_date, " +
-            "the end date as end_date, the start location name as start_location, the end loation name as end_location," +
-            " if the trip is possible as is_possible and a list of locations with their name, latitude, longitude, the dat you are there and possible" +
-            " activities as a list of names, with the field_names name, latitude, longitude, date, activities."+
-            "Give one location for each day from $startDate to $endDate. Check that all dates " +
+            "the end date as end_date, the start location name as start_location, the end location name as end_location," +
+            " if the trip is possible as is_possible and a list of locations with their name, latitude, longitude, the date you are there, " +
+            "a boolean that marks 2-3 must have stops that arent the start or end location " +
+            "and possible activities as a list of names, with the field_names name, latitude, longitude, date, must_have,  activities."+
+            "Make sure that there are multiple activities for each location. Give one location for each day from $startDate to $endDate. Check that all dates " +
             "are covered and that you arrive on the $endDate at $endLocation. Also check that the distances between locations are below 10 hours"
 }
